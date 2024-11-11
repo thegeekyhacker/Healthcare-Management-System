3d0d98ca089f05c81665e80a3d5ba3dc9c359cda
Utkarsh Tiwary <97603393+thegeekyhacker@users.noreply.github.com>
2024-11-11 23:27:40 +0530

    Changed MySQL database

--- createapp.py
+++ createapp.py
@@ -1,6 +1,8 @@
 import mysql.connector as sqltor
-con = sqltor.connect(host="localhost",user = "root",passwd = "admin",database = "hcm")
+con = sqltor.connect(host="localhost", user="root",
+                     passwd="admin", database="healthcare_management")
 cursor = con.cursor()
+
 
 def createapp():
     cursor.execute("Select * from appointments")
@@ -8,7 +10,7 @@ def createapp():
     count = len(data)
     app_id = count+1
     app_date = input("Enter the appointment date in YYYY-MM-DD format : ")
-    app_time =input("Enter the appointment time in HH:MM:SS format : ")
+    app_time = input("Enter the appointment time in HH:MM:SS format : ")
     pname = input("Enter the patient name : ")
     scheduled = 1
     canceled = 0
@@ -17,7 +19,8 @@ def createapp():
     department = input("Enter the department : ")
     patientid = input("Enter patient id : ")
     doctorid = input("Enter doctor id : ")
-    cursor.execute(f"insert into appointments values ({app_id},'{app_date}','{app_time}','{pname}',{scheduled},{canceled},{completed},'{specialization}','{department}','{patientid}','{doctorid}')")
+    cursor.execute(
+        f"insert into appointments values ({app_id},'{app_date}','{app_time}','{pname}',{scheduled},{canceled},{completed},'{specialization}','{department}','{patientid}','{doctorid}')")
     print("Appointment scheduled successfully")
     con.commit()
     con.close()

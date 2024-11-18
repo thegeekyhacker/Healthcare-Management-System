6934ef8bc9dc55273504d2e81d441f9b3bf0c74a
JayeshKankaria <101986220+JayeshKankaria@users.noreply.github.com>
2024-11-18 22:34:29 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,8 +1,11 @@
+import encode
 import mysql.connector as sqltor
-con = sqltor.connect(host="localhost",user ="root",passwd = "admin",database="hcm")
+con = sqltor.connect(host="localhost", user="root",
+                     passwd="admin", database="healthcare_management")
 cursor = con.cursor()
 
-def login(username,password):
+
+def login(username, password):
     query = f"select Password from credentials where User_Id = '{username}'"
     cursor.execute(query)
     data = cursor.fetchall()
@@ -11,10 +14,16 @@ def login(username,password):
         print("Kindly enter the correct username or register for a new account")
         return False
     else:
-        if password == data[0][0]:
+        temp_passwd = data[0][0]
+        # temp_passwd = encode.decrypt_password(temp_passwd)
+        if encode.verify(password, temp_passwd):
             print("Logged in Successfully")
             return True
         else:
             print("Incorrect Password")
-            print(f"Kindly enter the correct password for the username : {username}")
+            print(
+                f"Kindly enter the correct password for the username : {username}")
             return False
+
+
+# login()

d3e0a6144f3aef0861881d92e05b2ed928f7e371
JayeshKankaria <101986220+JayeshKankaria@users.noreply.github.com>
2024-11-18 22:41:16 +0530

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -1,12 +1,16 @@
 import mysql.connector as sqltor
-con = sqltor.connect(host="localhost",user = "root",passwd = "admin",database = "hcm")
+con = sqltor.connect(host="localhost", user="root",
+                     passwd="admin", database="healthcare_management")
 cursor = con.cursor()
 
+
 def schedule(userid):
-    cursor.execute(f"select day,type_of_work,time from doctorschedules where doctor_id = '{userid}'")
+    cursor.execute(
+        f"select day,type_of_work,time from doctorschedules where doctor_id = '{userid}'")
     data = cursor.fetchall()
-    cursor.execute(f"SELECT doctor_name from doctors where doctor_id = '{userid}'")
-    result=cursor.fetchall()
+    cursor.execute(
+        f"SELECT doctor_name from doctors where doctor_id = '{userid}'")
+    result = cursor.fetchall()
     # print(data)
     for i in data:
         text = f"{result[0][0]} has {i[1]} on {i[0]} from {i[2]}."

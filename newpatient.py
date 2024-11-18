04fbdd57b2bc22c1d49b165f54a060e49f083fb8
JayeshKankaria <101986220+JayeshKankaria@users.noreply.github.com>
2024-11-18 22:37:15 +0530

    Update newpatient.py

--- newpatient.py
+++ newpatient.py
@@ -1,6 +1,8 @@
 import mysql.connector as sqltor
-con = sqltor.connect(host="localhost",user = "root",passwd = "admin",database = "hcm")
+con = sqltor.connect(host="localhost", user="root",
+                     passwd="admin", database="healthcare_management")
 cursor = con.cursor()
+
 
 def newpatient():
     cursor.execute("Select * from appointments")
@@ -11,15 +13,16 @@ def newpatient():
     first_name = input("Enter the patient's first name :")
     middle_name = input("Enter the patient's middle name :")
     last_name = input("Enter the patient's last name :")
-    dob=input("Enter the patient's DOB :")
-    gender=input("Enter the patient's gender :")
-    address=input("Enter the patient's address :")
-    medHis=input("Enter the patient's medical history :")
-    email_id=input("Enter the patient's email_id :")
-    blood_group=input("Enter the patient's blood group :")
-    phoneno=input("Enter the patient's phone number :")
-    insuranceid=input("Enter the patient's Insurance id :")
-    cursor.execute(f"insert into patients values ('{pat_id}','{first_name}','{middle_name}','{last_name}','{dob}','{gender}','{address}','{medHis}','{email_id}','{blood_group}','{phoneno}','{insuranceid}')")
+    dob = input("Enter the patient's DOB :")
+    gender = input("Enter the patient's gender :")
+    address = input("Enter the patient's address :")
+    medHis = input("Enter the patient's medical history :")
+    email_id = input("Enter the patient's email_id :")
+    blood_group = input("Enter the patient's blood group :")
+    phoneno = input("Enter the patient's phone number :")
+    insuranceid = input("Enter the patient's Insurance id :")
+    cursor.execute(
+        f"insert into patients values ('{pat_id}','{first_name}','{middle_name}','{last_name}','{dob}','{gender}','{address}','{medHis}','{email_id}','{blood_group}','{phoneno}','{insuranceid}')")
     con.commit()
     con.close()
 # newpatient()
